f356a3c084f2bdd1a75f4a4a5a197e6e1bab3722
h662 <godh662@gmail.com>
2020-01-26 16:30:29 +0900

    react router && create navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Main from "./pages/main";
+import About from "./pages/about";
+import Project from "./pages/project";
+import Contact from "./pages/contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <Router>
+      <Header />
+      <Route path="/" exact component={Main} />
+      <Route path="/about" component={About} />
+      <Route path="/project" component={Project} />
+      <Route path="/contact" component={Contact} />
+      <Footer />
+    </Router>
   );
 }
 
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+
 import Button from "../components/Button";
 
-const Main = () => {
-  return (
-    <>
-      <h1>헬로우 리액트</h1>
-      <Button>버튼</Button>
-    </>
-  );
-};
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <h1>헬로우 리액트</h1>
+        <Button>버튼</Button>
+      </>
+    );
+  }
+}
 
 export default Main;
+
+//react
